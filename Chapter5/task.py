72268d05c75cec6c4b3dccf2595be5e039c50ddd
s21011 <s21011@std.it-college.ac.jp>
2023-06-22 16:33:05 +0900

    Chapter4kadai

--- Chapter5/task.py
+++ Chapter5/task.py
@@ -1,4 +1,12 @@
-data = ["b", "a", "c", "f", "e", "d", "g"]
+data = ["あ","さ","た","か","な"]
+
 print(data, "元のデータ")
 
-print(sorted(data), "ソート後のデータ")
+for i in range(0, 4):
+    m = i
+    for j in range(i+1, 5):
+        if data[j] < data[m]:
+            m = j
+    data[i], data[m] = data[m], data[i]
+
+print(data, "ソート後のデータ")
